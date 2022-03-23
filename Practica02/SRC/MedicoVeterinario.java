fac8e3c6fd74a105aeaa8a0f9223cd19f089e7c8
Sanfi0111 <santi@ciencias.unam.mx>
2022-03-23 00:53:48 -0600

    clase MedicoVeterinario.java

--- Practica02/SRC/MedicoVeterinario.java
+++ Practica02/SRC/MedicoVeterinario.java
@@ -10,7 +10,15 @@ class MedicoVeterinario{
     private String rfc;
     private String correo;
     private char turno;
-    
+    /**
+     * Constructor para un objeto MedicoVeterinario
+     * @param nombre_vet nombre del veterinario
+     * @param appellido_paterno apellido paterno del veterinario
+     * @param apellido_materno apellido materno del veterinario
+     * @param rfc rfc del veterinario
+     * @param correo correo del veterinario
+     * @param turno turno en el que trabaja el veterinario
+     */
     public MedicoVeterinario(String nombre_vet, String appellido_paterno, String apellido_materno, String rfc, String correo, char turno){
             this.nombre_Vet = nombre_vet;
             this.apellido_paterno = appellido_paterno;
@@ -20,40 +28,88 @@ class MedicoVeterinario{
             this.turno = turno;
 
     }
-
+    /**
+     * Regresa el nombre del veterinario
+     * @return nombre del veterinario
+     */
     public String getNombre_vet(){
         return this.nombre_Vet;
     }
+    /**
+     * Regresa el apellido paterno del veterinario
+     * @return apellido paterno del veterinario
+     */
     public String getApellidoPaterno(){
         return this.apellido_paterno;
     }
+    /**
+     * Regresa el apellido materno del veterinario
+     * @return apellido materno del veterinario
+     */
     public String getaApellido_Materno(){
         return this.apellido_materno;
     }
+    /**
+     * Regresa el rfc del veterinario
+     * @return rfc del veterinario
+     */
     public String getRfc(){
         return this.rfc;
     }
+     /**
+     * Regresa el correo del veterinario
+     * @return correo del veterinario
+     */
     public String getCorreo(){
         return this.correo;
     }
+    /**
+     * Regresa el turno del veterinario
+     * @return turno del veterinario
+     */
     public char getTurno(){
         return this.turno;
     }
+    /**
+     * Actualiza el valor de la variable de clase nombre_vet
+     * @param nombre_Vet el nuevo nombre del veterinario
+     */
     public void setNombre_Vet(String nombre_Vet){
         this.nombre_Vet = nombre_Vet;
     }
-    public void setApellido_Paterno(String appelido_Paterno){
-        this.apellido_paterno = appelido_Paterno;
+    /**
+     * Actualiza el valor de la variable de clase apellido_paterno
+     * @param apellido_paterno el nuevo apellido paterno del veterinario
+     */
+    public void setApellido_Paterno(String apellido_paterno){
+        this.apellido_paterno = apellido_paterno;
     }
+    /**
+     * Actualiza el valor de la variable de clase apellido_materno
+     * @param apellido_materno el nuevo apellido materno del veterinario
+     */
     public void setApellido_Materno (String apellido_Materno){
         this.apellido_materno = apellido_Materno;
     }
+    /**
+     * Actualiza el valor de la variable de clase rfc
+     * @param rfc el nuevo rfc del veterinario
+     */
     public void setRfc(String rfc){
         this.rfc = rfc;
     }
+    /**
+     * Actualiza el valor de la variable de clase turno
+     * @param turno el nuevo turno de veterinario
+     */
     public void setTurno(char turno){
         this.turno = turno;
     }
+    /**
+     * Apartir de un char turno, devuelve el turno al cual equivale en String
+     * @param turno el turno en forma char
+     * @return el turno en String que tiene un veterinario
+     */
     public String leeTurno(char turno){
         if(turno == 'M' || turno == 'm'){
             return "Matutino";
@@ -68,6 +124,9 @@ class MedicoVeterinario{
         throw new IllegalArgumentException("Entrada inválida");
         }
     }
+    /**
+     * Representación en cadena de un veterinario
+     */
     public String toString(){
         String nombre = "Nombre del veterinario: "+this.nombre_Vet+" "+ this.apellido_paterno +" "+this.apellido_materno;
         return nombre+", rfc: "+this.rfc+", correo: "+ this.correo+", turno: "+leeTurno(this.turno);
