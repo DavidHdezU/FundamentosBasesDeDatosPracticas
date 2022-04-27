49310ce434d475b717b0f103efa6cbc8ff31d16f
DavidHdezU <Dhdezu@ciencias.unam.mx>
2022-04-27 18:52:33 -0500

    Avance P06

---
+++ Practica06_DVDSJ/SQL/DDL.sql
@@ -0,0 +1,98 @@
+DROP SCHEMA IF EXISTS public CASCADE;
+CREATE SCHEMA public;
+
+CREATE TABLE cliente(
+	curp CHAR(18) NOT NULL CHECK(CHAR_LENGTH(curp) = 18) UNIQUE,
+	nombre VARCHAR(50) NOT NULL CHECK(nombre <> ''),
+	apellidoPaterno VARCHAR(50) NOT NULL CHECK(apellidoPaterno <> ''),
+	apellidoMaterno VARCHAR(50) NOT NULL CHECK(apellidoMaterno <> ''),
+	codigoPostal INT NOT NULL CHECK(codigoPostal between 10000 and 99999),
+    calle VARCHAR(50) NOT NULL CHECK(calle <> ''), 
+	estado VARCHAR(50) NOT NULL CHECK(estado <> ''),
+	telefono CHAR(10) NOT NULL CHECK(CHAR_LENGTH(telefono) = 10) UNIQUE,
+	PRIMARY KEY(curp)
+);
+
+CREATE TABLE mascota(
+	idMascota CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idMascota) = 18) UNIQUE,
+	curp CHAR(18) NOT NULL CHECK(CHAR_LENGTH(curp) = 18),
+    nombreDueño VARCHAR(50) NOT NULL CHECK(nombreDueño <> ''),
+	nombre VARCHAR(50) NOT NULL CHECK(nombre <> ''),
+    edad INT NOT NULL,
+	raza VARCHAR(50) NOT NULL CHECK(raza <> ''),
+	especie VARCHAR(50) NOT NULL CHECK(especie <> ''),
+    peso REAL NOT NULL,
+	PRIMARY KEY(idMascota),
+	FOREIGN KEY (curp) REFERENCES cliente(curp)
+);
+
+CREATE TABLE consultaEmergencia(
+    idConsulta CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idConsulta) = 18) UNIQUE,
+	sintomas VARCHAR(50) NOT NULL CHECK(sintomas <> ''),
+	procedimiento VARCHAR(50) NOT NULL CHECK(procedimiento <> ''),
+    PRIMARY KEY(idConsulta)
+
+);
+
+CREATE TABLE codigo(
+    idConsulta CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idConsulta) = 18) UNIQUE,
+	codigo CHAR(18) NOT NULL CHECK(CHAR_LENGTH(codigo) = 18) UNIQUE,
+    PRIMARY KEY(codigo),
+    FOREIGN KEY(idConsulta) REFERENCES consultaEmergencia(idConsulta)
+);
+
+CREATE TABLE requerirEmergencia(
+	idConsulta CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idConsulta) = 18),
+	idMascota CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idMascota) = 18),
+    PRIMARY KEY(idConsulta,idMascota),
+	FOREIGN KEY(idConsulta) REFERENCES consultaEmergencia(idConsulta),
+	FOREIGN KEY(idMascota) REFERENCES mascota(idMascota)
+);
+
+CREATE TABLE consultaNormal(
+    idConsulta CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idConsulta) = 18) UNIQUE,
+    motivos VARCHAR(50) NOT NULL CHECK(motivos <> ''),
+    proximaRevision DATE,
+    estado VARCHAR(20) NOT NULL CHECK(estado <> ''),
+    medicamentosRecetados VARCHAR(50) CHECK(motivos <> ''),
+    PRIMARY KEY(idConsulta)
+);
+
+CREATE TABLE requerirNormal(
+	idConsulta CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idConsulta) = 18),
+	idMascota CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idMascota) = 18),
+    PRIMARY KEY(idConsulta,idMascota),
+	FOREIGN KEY(idConsulta) REFERENCES consultaNormal(idConsulta),
+	FOREIGN KEY(idMascota) REFERENCES mascota(idMascota)
+);
+
+
+CREATE TABLE recibo(
+    idRecibo CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idRecibo) = 18) UNIQUE,
+    nombreDueño VARCHAR(50) NOT NULL CHECK(nombreDueño <> ''),
+	nombreMascota VARCHAR(50) NOT NULL CHECK(nombreMascota <> ''),
+	nombreTrabajador VARCHAR(50) NOT NULL CHECK(nombreTrabajador <> ''),
+    PRIMARY KEY(idRecibo)
+);
+
+CREATE TABLE generaReciboNormal(
+	idConsulta CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idConsulta) = 18),
+	idRecibo CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idRecibo) = 18),
+    PRIMARY KEY(idConsulta,idRecibo),
+	FOREIGN KEY(idConsulta) REFERENCES consultaNormal(idConsulta),
+	FOREIGN KEY(idRecibo) REFERENCES recibo(idRecibo)
+);
+
+CREATE TABLE generaReciboEmergencia(
+	idConsulta CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idConsulta) = 18),
+	idRecibo CHAR(18) NOT NULL CHECK(CHAR_LENGTH(idRecibo) = 18),
+    PRIMARY KEY(idConsulta,idRecibo),
+	FOREIGN KEY(idConsulta) REFERENCES consultaEmergencia(idConsulta),
+	FOREIGN KEY(idRecibo) REFERENCES recibo(idRecibo)
+);
+
+INSERT INTO cliente VALUES ('12345678901234567A', 'David', 'Hernández', 'Uriostegui', 10000, 'Ignacio Allende', 'Guanajuato', '4731241727') 
+
+
+
+
