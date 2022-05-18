ad937833d36801d38a8fbb2fadf63b75ef422a03
DavidHdezU <Dhdezu@ciencias.unam.mx>
2022-05-18 17:23:50 -0500

    Avance P09

---
+++ Practica09/Practica09/src/main/java/transportecolectivo/practica09/Modelo/Mascota.java
@@ -0,0 +1,136 @@
+package transportecolectivo.practica09.Modelo;
+
+public class Mascota {
+    private String idMascota;
+    private String curp;
+    private String nombreDueño;
+    private String nombre;
+    private int edad;
+    private String raza;
+    private String especie;
+    private float peso;
+
+    public Mascota(String idMascota, String curp, String nombreDueño, String nombre, int edad, String raza, String especie, float peso){
+        this.idMascota = idMascota;
+        this.curp = curp;
+        this.nombreDueño = nombreDueño;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.raza = raza;
+        this.especie = especie;
+        this.peso = peso;
+    }
+
+    /**
+     * @return String return the idMascota
+     */
+    public String getIdMascota() {
+        return idMascota;
+    }
+
+    /**
+     * @param idMascota the idMascota to set
+     */
+    public void setIdMascota(String idMascota) {
+        this.idMascota = idMascota;
+    }
+
+    /**
+     * @return String return the curp
+     */
+    public String getCurp() {
+        return curp;
+    }
+
+    /**
+     * @param curp the curp to set
+     */
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    /**
+     * @return String return the nombreDueño
+     */
+    public String getNombreDueño() {
+        return nombreDueño;
+    }
+
+    /**
+     * @param nombreDueño the nombreDueño to set
+     */
+    public void setNombreDueño(String nombreDueño) {
+        this.nombreDueño = nombreDueño;
+    }
+
+    /**
+     * @return String return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return int return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * @return String return the raza
+     */
+    public String getRaza() {
+        return raza;
+    }
+
+    /**
+     * @param raza the raza to set
+     */
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    /**
+     * @return String return the especie
+     */
+    public String getEspecie() {
+        return especie;
+    }
+
+    /**
+     * @param especie the especie to set
+     */
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    /**
+     * @return float return the peso
+     */
+    public float getPeso() {
+        return peso;
+    }
+
+    /**
+     * @param peso the peso to set
+     */
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+}
