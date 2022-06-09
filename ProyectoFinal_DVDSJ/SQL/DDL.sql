cd77e7bace55bb6d341eb12c0ff45894aa1656b5
DiXap <dpadlara@ciencias.unam.mx>
2022-06-09 06:41:59 -0500

    Segunda version del script DDL

--- ProyectoFinal_DVDSJ/SQL/DDL.sql
+++ ProyectoFinal_DVDSJ/SQL/DDL.sql
@@ -1,3 +1,8 @@
+-- vim:fileencoding=utf-8:foldmethod=marker
+/*
+ * Author: Diego Padilla
+ * Version: 22.5.9
+ */
 DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA public;
 
@@ -5,64 +10,290 @@ CREATE SCHEMA public;
 CREATE TABLE taqueria (
     id INT NOT NULL UNIQUE,
     RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
-    nombre   VARCHAR(100) NOT NULL CHECK(nombre <> ''),
-    calle    VARCHAR(50)  NOT NULL,
-    numero   VARCHAR(10)  NOT NULL,
-    estado   VARCHAR(50)  NOT NULL CHECK(estado ~* '^[a-z]+$'),
-    cpostal  VARCHAR(5)   NOT NULL CHECK(cpostal ~* '^[0-9]+$' AND  CHAR_LENGTH(cpostal) = 5),
-    telefono VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(telefono) = 10) UNIQUE,
-    email    VARCHAR(320) NOT NULL UNIQUE,
+    Nombre   VARCHAR(100) NOT NULL CHECK(Nombre <> ''),
+    -- Direccion
+    Calle    VARCHAR(50)  NOT NULL,
+    Numero   VARCHAR(10)  NOT NULL,
+    Estado   VARCHAR(50)  NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    C_Postal VARCHAR(5)   NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
+    -- Contacto
+    Telefono VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
+    Email    VARCHAR(320) NOT NULL UNIQUE,
     
     PRIMARY KEY(id)
 );
 
 
+-- Empleados    -- {{{
 CREATE TABLE parrillero (
     RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
-    idTaqueria INT  NOT NULL,
+    id_Taqueria INT NOT NULL,
     -- Nombre completo
-    nombre   VARCHAR(50) NOT NULL CHECK(nombre <> '' AND nombre ~* '^[a-z]+$'),
-    APaterno VARCHAR(50) NOT NULL CHECK(APaterno <> '' AND APaterno ~* '^[a-z]+$'),
-    AMaterno VARCHAR(50) NOT NULL CHECK(AMaterno <> '' AND AMaterno ~* '^[a-z]+$'),
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
+    A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
     -- Direccion
-    calle   VARCHAR(50) NOT NULL,
-    numero  VARCHAR(10) NOT NULL,
-    estado  VARCHAR(50) NOT NULL CHECK(estado ~* '^[a-z]+$'),
-    cpostal VARCHAR(5)  NOT NULL CHECK(cpostal ~* '^[0-9]+$' AND  CHAR_LENGTH(cpostal) = 5),
+    Calle    VARCHAR(50) NOT NULL,
+    Numero   VARCHAR(10) NOT NULL,
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
 
-    edad INT NOT NULL CHECK(edad >= 18),
+    Edad INT NOT NULL CHECK(edad >= 18),
     NSS        VARCHAR(11)  NOT NULL CHECK(NSS ~* '^[a-z0-9]+$' AND CHAR_LENGTH(NSS) = 11) UNIQUE,
-    antiguedad DATE NOT NULL,
-    telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(telefono) = 10) UNIQUE,
-    email      VARCHAR(320) NOT NULL UNIQUE,
+    Antiguedad DATE NOT NULL CHECK(Antiguedad <= NOW()),
+    -- Contacto
+    Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
+    Email      VARCHAR(320) NOT NULL UNIQUE,
 
     PRIMARY KEY(RFC),
-    FOREING KEY(idTaqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE NO ACTION
+    FOREING KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 CREATE TABLE taquero (
     RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
-    idTaqueria INT  NOT NULL,
+    id_Taqueria INT NOT NULL,
     -- Nombre completo
-    nombre   VARCHAR(50) NOT NULL CHECK(nombre <> '' AND nombre ~* '^[a-z]+$'),
-    APaterno VARCHAR(50) NOT NULL CHECK(APaterno <> '' AND APaterno ~* '^[a-z]+$'),
-    AMaterno VARCHAR(50) NOT NULL CHECK(AMaterno <> '' AND AMaterno ~* '^[a-z]+$'),
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
+    A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
     -- Direccion
-    calle   VARCHAR(50) NOT NULL,
-    numero  VARCHAR(10) NOT NULL,
-    estado  VARCHAR(50) NOT NULL CHECK(estado ~* '^[a-z]+$'),
-    cpostal VARCHAR(5)  NOT NULL CHECK(cpostal ~* '^[0-9]+$' AND  CHAR_LENGTH(cpostal) = 5),
+    Calle    VARCHAR(50) NOT NULL,
+    Numero   VARCHAR(10) NOT NULL,
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
 
-    edad INT NOT NULL CHECK(edad >= 18),
+    Edad INT NOT NULL CHECK(edad >= 18),
     NSS        VARCHAR(11)  NOT NULL CHECK(NSS ~* '^[a-z0-9]+$' AND CHAR_LENGTH(NSS) = 11) UNIQUE,
-    antiguedad DATE NOT NULL,
-    telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(telefono) = 10) UNIQUE,
-    email      VARCHAR(320) NOT NULL UNIQUE,
+    Antiguedad DATE NOT NULL CHECK(Antiguedad <= NOW()),
+    -- Contacto
+    Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
+    Email      VARCHAR(320) NOT NULL UNIQUE,
 
     PRIMARY KEY(RFC),
-    FOREING KEY(idTaqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE NO ACTION
+    FOREING KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+
+CREATE TABLE mesero (
+    RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
+    id_Taqueria INT NOT NULL,
+    -- Nombre completo
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
+    A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
+    -- Direccion
+    Calle    VARCHAR(50) NOT NULL,
+    Numero   VARCHAR(10) NOT NULL,
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
+
+    Edad INT NOT NULL CHECK(edad >= 18),
+    NSS        VARCHAR(11)  NOT NULL CHECK(NSS ~* '^[a-z0-9]+$' AND CHAR_LENGTH(NSS) = 11) UNIQUE,
+    Antiguedad DATE NOT NULL CHECK(Antiguedad <= NOW()),
+    -- Contacto
+    Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
+    Email      VARCHAR(320) NOT NULL UNIQUE,
+
+    PRIMARY KEY(RFC),
+    FOREING KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+CREATE TABLE cajero (
+    RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
+    id_Taqueria INT NOT NULL,
+    -- Nombre completo
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
+    A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
+    -- Direccion
+    Calle    VARCHAR(50) NOT NULL,
+    Numero   VARCHAR(10) NOT NULL,
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
+
+    Edad INT NOT NULL CHECK(edad >= 18),
+    NSS        VARCHAR(11)  NOT NULL CHECK(NSS ~* '^[a-z0-9]+$' AND CHAR_LENGTH(NSS) = 11) UNIQUE,
+    Antiguedad DATE NOT NULL CHECK(Antiguedad <= NOW()),
+    -- Contacto
+    Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
+    Email      VARCHAR(320) NOT NULL UNIQUE,
+
+    PRIMARY KEY(RFC),
+    FOREING KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+CREATE TABLE tortillero (
+    RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
+    id_Taqueria INT NOT NULL,
+    -- Nombre completo
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
+    A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
+    -- Direccion
+    Calle    VARCHAR(50) NOT NULL,
+    Numero   VARCHAR(10) NOT NULL,
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
+
+    Edad INT NOT NULL CHECK(edad >= 18),
+    NSS        VARCHAR(11)  NOT NULL CHECK(NSS ~* '^[a-z0-9]+$' AND CHAR_LENGTH(NSS) = 11) UNIQUE,
+    Antiguedad DATE NOT NULL CHECK(Antiguedad <= NOW()),
+    -- Contacto
+    Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
+    Email      VARCHAR(320) NOT NULL UNIQUE,
+
+    PRIMARY KEY(RFC),
+    FOREING KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+CREATE TABLE repartidor (
+    RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
+    id_Taqueria INT NOT NULL,
+    -- Nombre completo
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
+    A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
+    -- Direccion
+    Calle    VARCHAR(50) NOT NULL,
+    Numero   VARCHAR(10) NOT NULL,
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
+
+    Edad INT NOT NULL CHECK(edad >= 18),
+    NSS        VARCHAR(11)  NOT NULL CHECK(NSS ~* '^[a-z0-9]+$' AND CHAR_LENGTH(NSS) = 11) UNIQUE,
+    Antiguedad DATE NOT NULL CHECK(Antiguedad <= NOW()),
+    -- Contacto
+    Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
+    Email      VARCHAR(320) NOT NULL UNIQUE,
+
+    -- Transporte
+    tieneTransportePropio BOOLEAN NOT NULL, 
+    marca  VARCHAR(50),
+    modelo VARCHAR(50),
+    tipo   VARCHAR(50),
+
+    PRIMARY KEY(RFC),
+    FOREING KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+-- }}}
+
+
+CREATE TABLE cliente (
+    CURP VARCHAR(18) NOT NULL CHECK(CURP ~* '^[a-z0-9]+$') UNIQUE,
+    id_Taqueria INT NOT NULL,
+    -- Nombre completo
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
+    A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
+    -- Direccion
+    Calle    VARCHAR(50) NOT NULL,
+    Numero   VARCHAR(10) NOT NULL,
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
+    -- Contacto
+    Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
+    Email      VARCHAR(320) NOT NULL UNIQUE,
+
+    A_Domicilio BOOLEAN NOT NULL,
+    Puntos_Taquero_Corazon DECIMAL NOT NULL CHECK(Puntos_Taquero_Corazon >= 0),
+
+    PRIMARY KEY(CURP),
+    FOREING KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+
+CREATE TABLE ticket (
+    id VARCHAR(4) NOT NULL CHECK(id ~* '^[a-z0-9]+$' AND CHAR_LENGTH(id) = 4) UNIQUE,
+    id_Cliente VARCHAR(18) NOT NULL,
+    Fecha DATETIME NOT NULL CHECK(Fecha <= NOW()),
+    RFC_Mesero VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$'), -- TODO: Asegurar integridad referencial con los RFC de mesero
+    -- Metodo de pago
+    Es_Efectivo BOOLEAN NOT NULL,
+    Es_Tarjeta  BOOLEAN NOT NULL,
+    Es_Puntos   BOOLEAN NOT NULL,
+    -- Monto
+    Total DECIMAL NOT NULL CHECK(Total >= 0),
+
+    PRIMARY KEY(id),
+    FOREING KEY(id_Cliente) REFERENCES cliente(CURP) ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+
+-- Menu     -- {{{
+CREATE TABLE categoria (
+    id INT NOT NULL UNIQUE,
+    Nombre VARCHAR(50) CHECK(Nombre <> ''),
+
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE item (
+    id INT NOT NULL UNIQUE,
+    id_Categoria   INT  NOT NULL,
+    Nombre VARCHAR(50)  NOT NULL CHECK(Nombre <> ''),
+    Descripcion TEXT    NOT NULL,
+    Precio      DECIMAL NOT NULL CHECK(Precio >= 0),
+
+    PRIMARY KEY(id),
+    FOREING KEY(id_Categoria) REFERENCES categoria(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+CREATE TABLE salsa (
+    id INT NOT NULL UNIQUE,
+    id_Categoria   INT NOT NULL,
+    Nombre VARCHAR(50) NOT NULL CHECK(Nombre <> ''),
+    Picor  VARCHAR(50) NOT NULL CHECK(Picor <> ''),
+    Recomendacion TEXT NOT NULL CHECK(Recomendacion <> ''),
+    -- Precios
+    Precio250  DECIMAL NOT NULL CHECK(Precio250 >= 0),
+    Precio500  DECIMAL NOT NULL CHECK(Precio500 >= 0),
+    Precio1000 DECIMAL NOT NULL CHECK(Precio1000 >= 0),
+
+    PRIMARY KEY(id),
+    FOREING KEY(id_Categoria) REFERENCES categoria(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+CREATE TABLE ingrediente (
+    id INT NOT NULL UNIQUE,
+    Nombre VARCHAR(50) NOT NULL CHECK(Nombre <> ''),
+    Tipo   VARCHAR(50) NOT NULL CHECK(Nombre <> ''),
+    Precio DECIMAL NOT NULL CHECK(Precio250 >= 0),
+
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE utilizar (
+    id_Item INT NOT NULL,
+    id_Ingrediente INT NOT NULL,
+    
+    Cantidad INT NOT NULL CHECK(Cantidad >= 0),
+
+    PRIMARY KEY(id_Item, id_Ingrediente),
+    FOREING KEY(id_Item) REFERENCES item(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREING KEY(id_Ingrediente) REFERENCES ingrediente(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+CREATE TABLE utilizar_en_salsa (
+    id_Salsa INT NOT NULL,
+    id_Ingrediente INT NOT NULL,
+    
+    Cantidad INT NOT NULL CHECK(Cantidad >= 0),
+
+    PRIMARY KEY(id_Salsa, id_Ingrediente),
+    FOREING KEY(id_Salsa) REFERENCES salsa(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREING KEY(id_Ingrediente) REFERENCES ingrediente(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- }}}
 
 ----------- Comments -----------
 
@@ -76,4 +307,44 @@ COMMENT ON TABLE parrillero IS 'Almacena la informacion de todos los empleados p
 
 -- Comentarios de Taquero      -- {{{ 
 COMMENT ON TABLE taquero IS 'Almacena la informacion de todos los empleados taqueros';
+-- }}}
+
+-- Comentarios de Mesero      -- {{{ 
+COMMENT ON TABLE mesero IS 'Almacena la informacion de todos los empleados meseros';
+-- }}}
+
+-- Comentarios de Cajero      -- {{{ 
+COMMENT ON TABLE cajero IS 'Almacena la informacion de todos los empleados cajeros';
+-- }}}
+
+-- Comentarios de Tortillero  -- {{{ 
+COMMENT ON TABLE tortillero IS 'Almacena la informacion de todos los empleados tortilleros';
+-- }}}
+
+-- Comentarios de Repartidor  -- {{{ 
+COMMENT ON TABLE repartidor IS 'Almacena la informacion de todos los empleados repartidores';
+-- }}}
+
+-- Comentarios de Cliente     -- {{{ 
+COMMENT ON TABLE cliente IS 'Almacena la informacion de todos los clientes';
+-- }}}
+
+-- Comentarios de Ticket      -- {{{ 
+COMMENT ON TABLE ticket IS 'Almacena la informacion de todos los tickets de consumo';
+-- }}}
+
+-- Comentarios de Categoria   -- {{{ 
+COMMENT ON TABLE categoria IS 'Almacena la informacion de las categorias del menu del restaurante';
+-- }}}
+
+-- Comentarios de Item        -- {{{ 
+COMMENT ON TABLE item IS 'Almacena la informacion de todos los items del menu';
+-- }}}
+
+-- Comentarios de Salsa       -- {{{ 
+COMMENT ON TABLE salsa IS 'Almacena la informacion de las salsas';
+-- }}}
+
+-- Comentarios de Ingrediente -- {{{ 
+COMMENT ON TABLE ingrediente IS 'Almacena la informacion relevante de los ingredientes para cada plato';
 -- }}}
