3ac76a6b965c5c692ffe861b7692c62b1fed3b8d
DiXap <dpadlara@ciencias.unam.mx>
2022-06-10 00:00:42 -0500

    Tigger para manejar a los clientes default

---
+++ ProyectoFinal_DVDSJ/SQL/Triggers.sql
@@ -0,0 +1,42 @@
+/*
+ * Author: Diego Padilla
+ * Version: 22.5.9
+ */
+
+CREATE OR REPLACE FUNCTION default_data()
+    RETURNS TRIGGER AS 
+    $$
+    BEGIN
+        IF (TG_OP = 'INSERT') THEN 
+            INSERT INTO 
+                cliente(CURP, id_Taqueria, Nombre, A_Paterno, A_Materno,
+                        Calle, Numero, Estado, C_Postal, Telefono, 
+                        Email, A_Domicilio, Puntos_Taquero_Corazon)
+            VALUES 
+                (NEW.RFC, NEW.id, NEW.Nombre, 'Ricos', 'Tacos', NEW.Calle,
+                NEW.Numero, NEW.Estado, NEW.C_Postal, NEW.Telefono
+                NEW.Email, FALSE, 0);
+
+        ELSIF (TG_OP = 'UPDATE') THEN 
+            -- Eliminar cliente default anterior
+            DELETE FROM cliente AS clnt WHERE clnt.CURP = OLD.RFC;
+            -- Crear un cliente default nuevo
+            INSERT INTO 
+                cliente(CURP, id_Taqueria, Nombre, A_Paterno, A_Materno,
+                        Calle, Numero, Estado, C_Postal, Telefono, 
+                        Email, A_Domicilio, Puntos_Taquero_Corazon)
+            VALUES 
+                (NEW.RFC, NEW.id, NEW.Nombre, 'Ricos', 'Tacos', NEW.Calle,
+                NEW.Numero, NEW.Estado, NEW.C_Postal, NEW.Telefono
+                NEW.Email, FALSE, 0);
+        END IF;
+        RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER usuario_default
+    AFTER INSERT OR UPDATE 
+    ON taqueria
+    FOR EACH ROW
+    EXECUTE PROCEDURE default_data();
