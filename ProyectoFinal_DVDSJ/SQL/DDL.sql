46dcbdfc20e57b9259c922000c5d126fe0c8b6ff
DiXap <dpadlara@ciencias.unam.mx>
2022-06-11 18:51:28 -0500

    Cambio de llave primaria para empleados

--- ProyectoFinal_DVDSJ/SQL/DDL.sql
+++ ProyectoFinal_DVDSJ/SQL/DDL.sql
@@ -10,32 +10,33 @@ CREATE SCHEMA public;
 CREATE TABLE taqueria (
     id INT NOT NULL UNIQUE,
     RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
-    Nombre   VARCHAR(100) NOT NULL CHECK(Nombre <> ''),
+    Nombre   VARCHAR(100) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z\s]+$'),
     -- Direccion
     Calle    VARCHAR(50)  NOT NULL,
     Numero   VARCHAR(10)  NOT NULL,
-    Estado   VARCHAR(50)  NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    Estado   VARCHAR(50)  NOT NULL CHECK(Estado   ~* '^[a-z]+$'),
     C_Postal VARCHAR(5)   NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
     -- Contacto
     Telefono VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
     Email    VARCHAR(320) NOT NULL UNIQUE,
     
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 
 -- Empleados    -- {{{
 CREATE TABLE parrillero (
-    RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
-    id_Taqueria INT NOT NULL,
+    CURP VARCHAR(18) NOT NULL CHECK(CURP ~* '^[a-z0-9]+$') UNIQUE,
+    RFC  VARCHAR(13) NOT NULL CHECK(RFC  ~* '^[a-z0-9]+$') UNIQUE,
+    id_Taqueria  INT NOT NULL,
     -- Nombre completo
-    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre    <> '' AND Nombre    ~* '^[a-z]+$'),
     A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
     A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
     -- Direccion
     Calle    VARCHAR(50) NOT NULL,
     Numero   VARCHAR(10) NOT NULL,
-    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado   ~* '^[a-z]+$'),
     C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
 
     Edad INT NOT NULL CHECK(edad >= 18),
@@ -45,22 +46,23 @@ CREATE TABLE parrillero (
     Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
     Email      VARCHAR(320) NOT NULL UNIQUE,
 
-    PRIMARY KEY (RFC),
+    PRIMARY KEY (CURP),
     FOREIGN KEY (id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 CREATE TABLE taquero (
-    RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
-    id_Taqueria INT NOT NULL,
+    CURP VARCHAR(18) NOT NULL CHECK(CURP ~* '^[a-z0-9]+$') UNIQUE,
+    RFC  VARCHAR(13) NOT NULL CHECK(RFC  ~* '^[a-z0-9]+$') UNIQUE,
+    id_Taqueria  INT NOT NULL,
     -- Nombre completo
-    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre    <> '' AND Nombre    ~* '^[a-z]+$'),
     A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
     A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
     -- Direccion
     Calle    VARCHAR(50) NOT NULL,
     Numero   VARCHAR(10) NOT NULL,
-    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado   ~* '^[a-z]+$'),
     C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
 
     Edad INT NOT NULL CHECK(edad >= 18),
@@ -70,22 +72,23 @@ CREATE TABLE taquero (
     Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
     Email      VARCHAR(320) NOT NULL UNIQUE,
 
-    PRIMARY KEY(RFC),
-    FOREIGN KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (CURP),
+    FOREIGN KEY (id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 CREATE TABLE mesero (
-    RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
-    id_Taqueria INT NOT NULL,
+    CURP VARCHAR(18) NOT NULL CHECK(CURP ~* '^[a-z0-9]+$') UNIQUE,
+    RFC  VARCHAR(13) NOT NULL CHECK(RFC  ~* '^[a-z0-9]+$') UNIQUE,
+    id_Taqueria  INT NOT NULL,
     -- Nombre completo
-    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre    <> '' AND Nombre    ~* '^[a-z]+$'),
     A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
     A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
     -- Direccion
     Calle    VARCHAR(50) NOT NULL,
     Numero   VARCHAR(10) NOT NULL,
-    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado   ~* '^[a-z]+$'),
     C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
 
     Edad INT NOT NULL CHECK(edad >= 18),
@@ -95,22 +98,23 @@ CREATE TABLE mesero (
     Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
     Email      VARCHAR(320) NOT NULL UNIQUE,
 
-    PRIMARY KEY(RFC),
-    FOREIGN KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (CURP),
+    FOREIGN KEY (id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 CREATE TABLE cajero (
-    RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
-    id_Taqueria INT NOT NULL,
+    CURP VARCHAR(18) NOT NULL CHECK(CURP ~* '^[a-z0-9]+$') UNIQUE,
+    RFC  VARCHAR(13) NOT NULL CHECK(RFC  ~* '^[a-z0-9]+$') UNIQUE,
+    id_Taqueria  INT NOT NULL,
     -- Nombre completo
-    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre    <> '' AND Nombre    ~* '^[a-z]+$'),
     A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
     A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
     -- Direccion
     Calle    VARCHAR(50) NOT NULL,
     Numero   VARCHAR(10) NOT NULL,
-    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado   ~* '^[a-z]+$'),
     C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
 
     Edad INT NOT NULL CHECK(edad >= 18),
@@ -120,22 +124,23 @@ CREATE TABLE cajero (
     Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
     Email      VARCHAR(320) NOT NULL UNIQUE,
 
-    PRIMARY KEY(RFC),
-    FOREIGN KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (CURP),
+    FOREIGN KEY (id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 CREATE TABLE tortillero (
-    RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
-    id_Taqueria INT NOT NULL,
+    CURP VARCHAR(18) NOT NULL CHECK(CURP ~* '^[a-z0-9]+$') UNIQUE,
+    RFC  VARCHAR(13) NOT NULL CHECK(RFC  ~* '^[a-z0-9]+$') UNIQUE,
+    id_Taqueria  INT NOT NULL,
     -- Nombre completo
-    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre    <> '' AND Nombre    ~* '^[a-z]+$'),
     A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
     A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
     -- Direccion
     Calle    VARCHAR(50) NOT NULL,
     Numero   VARCHAR(10) NOT NULL,
-    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado   ~* '^[a-z]+$'),
     C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
 
     Edad INT NOT NULL CHECK(edad >= 18),
@@ -145,22 +150,23 @@ CREATE TABLE tortillero (
     Telefono   VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,
     Email      VARCHAR(320) NOT NULL UNIQUE,
 
-    PRIMARY KEY(RFC),
-    FOREIGN KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (CURP),
+    FOREIGN KEY (id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 CREATE TABLE repartidor (
-    RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
-    id_Taqueria INT NOT NULL,
+    CURP VARCHAR(18) NOT NULL CHECK(CURP ~* '^[a-z0-9]+$') UNIQUE,
+    RFC  VARCHAR(13) NOT NULL CHECK(RFC  ~* '^[a-z0-9]+$') UNIQUE,
+    id_Taqueria  INT NOT NULL,
     -- Nombre completo
-    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
+    Nombre    VARCHAR(50) NOT NULL CHECK(Nombre    <> '' AND Nombre    ~* '^[a-z]+$'),
     A_Paterno VARCHAR(50) NOT NULL CHECK(A_Paterno <> '' AND A_Paterno ~* '^[a-z]+$'),
     A_Materno VARCHAR(50) NOT NULL CHECK(A_Materno <> '' AND A_Materno ~* '^[a-z]+$'),
     -- Direccion
     Calle    VARCHAR(50) NOT NULL,
     Numero   VARCHAR(10) NOT NULL,
-    Estado   VARCHAR(50) NOT NULL CHECK(Estado ~* '^[a-z]+$'),
+    Estado   VARCHAR(50) NOT NULL CHECK(Estado   ~* '^[a-z]+$'),
     C_Postal VARCHAR(5)  NOT NULL CHECK(C_Postal ~* '^[0-9]+$' AND  CHAR_LENGTH(C_Postal) = 5),
 
     Edad INT NOT NULL CHECK(edad >= 18),
@@ -176,8 +182,8 @@ CREATE TABLE repartidor (
     modelo VARCHAR(50),
     tipo   VARCHAR(50),
 
-    PRIMARY KEY(RFC),
-    FOREIGN KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (CURP),
+    FOREIGN KEY (id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 -- }}}
 
@@ -201,8 +207,8 @@ CREATE TABLE cliente (
     A_Domicilio BOOLEAN NOT NULL,
     Puntos_Taquero_Corazon DECIMAL NOT NULL CHECK(Puntos_Taquero_Corazon >= 0),
 
-    PRIMARY KEY(CURP),
-    FOREIGN KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE NO ACTION
+    PRIMARY KEY (CURP),
+    FOREIGN KEY (id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
 
@@ -218,26 +224,26 @@ CREATE TABLE ticket (
     -- Monto
     Total DECIMAL NOT NULL CHECK(Total >= 0),
 
-    PRIMARY KEY(id),
-    FOREIGN KEY(id_Cliente) REFERENCES cliente(CURP) ON UPDATE CASCADE ON DELETE NO ACTION
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_Cliente) REFERENCES cliente(CURP) ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
 
 -- Menu     -- {{{
 CREATE TABLE categoria (
     id INT NOT NULL UNIQUE,
-    Nombre VARCHAR(50) CHECK(Nombre <> ''),
+    Nombre VARCHAR(50) NOT NULL CHECK(Nombre <> ''),
 
     PRIMARY KEY(id)
 );
 
 CREATE TABLE organizar_menu (
-    id_Taqueria INT NOT NULL,
+    id_Taqueria  INT NOT NULL,
     id_Categoria INT NOT NULL,
 
-    PRIMARY KEY(id_Taqueria, id_Categoria),
-    FOREIGN KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY(id_Ingrid_Categoriaediente) REFERENCES categoria(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (id_Taqueria, id_Categoria),
+    FOREIGN KEY (id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (id_Ingrid_Categoriaediente) REFERENCES categoria(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
@@ -248,8 +254,8 @@ CREATE TABLE item (
     Descripcion TEXT    NOT NULL,
     Precio      DECIMAL NOT NULL CHECK(Precio >= 0),
 
-    PRIMARY KEY(id),
-    FOREIGN KEY(id_Categoria) REFERENCES categoria(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_Categoria) REFERENCES categoria(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
@@ -264,8 +270,8 @@ CREATE TABLE salsa (
     Precio500  DECIMAL NOT NULL CHECK(Precio500 >= 0),
     Precio1000 DECIMAL NOT NULL CHECK(Precio1000 >= 0),
 
-    PRIMARY KEY(id),
-    FOREIGN KEY(id_Categoria) REFERENCES categoria(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_Categoria) REFERENCES categoria(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
@@ -273,93 +279,90 @@ CREATE TABLE ingrediente (
     id INT NOT NULL UNIQUE,
     Nombre VARCHAR(50) NOT NULL CHECK(Nombre <> ''),
     Tipo   VARCHAR(50) NOT NULL CHECK(Nombre <> ''),
-    Precio DECIMAL NOT NULL CHECK(Precio >= 0),
+    Precio DECIMAL     NOT NULL CHECK(Precio >= 0),
 
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 
 CREATE TABLE utilizar (
-    id_Item INT NOT NULL,
+    id_Item        INT NOT NULL,
     id_Ingrediente INT NOT NULL,
-    
-    Cantidad INT NOT NULL CHECK(Cantidad >= 0),
+    Cantidad       INT NOT NULL CHECK(Cantidad >= 0),
 
-    PRIMARY KEY(id_Item, id_Ingrediente),
-    FOREIGN KEY(id_Item) REFERENCES item(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY(id_Ingrediente) REFERENCES ingrediente(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (id_Item, id_Ingrediente),
+    FOREIGN KEY (id_Item) REFERENCES item(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (id_Ingrediente) REFERENCES ingrediente(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 CREATE TABLE utilizar_en_salsa (
-    id_Salsa INT NOT NULL,
+    id_Salsa       INT NOT NULL,
     id_Ingrediente INT NOT NULL,
-    
-    Cantidad INT NOT NULL CHECK(Cantidad >= 0),
+    Cantidad       INT NOT NULL CHECK(Cantidad >= 0),
 
-    PRIMARY KEY(id_Salsa, id_Ingrediente),
-    FOREIGN KEY(id_Salsa) REFERENCES salsa(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY(id_Ingrediente) REFERENCES ingrediente(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (id_Salsa, id_Ingrediente),
+    FOREIGN KEY (id_Salsa) REFERENCES salsa(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (id_Ingrediente) REFERENCES ingrediente(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE registro_historico (
     id_Registro INT NOT NULL UNIQUE,
-    id_Item INT NOT NULL UNIQUE,
+    id_Item     INT NOT NULL UNIQUE,
 
-    Precio DECIMAL NOT NULL CHECK(Precio >= 0),
+    Precio   DECIMAL NOT NULL CHECK(Precio >= 0),
     Pinsumos DECIMAL NOT NULL CHECK(Pinsumos >= 0),
-    Fecha DATE NOT NULL CHECK(Fecha <= NOW()),
+    Fecha    DATE    NOT NULL CHECK(Fecha <= NOW()),
     
-    PRIMARY KEY(id_Registro),
-    FOREIGN KEY(id_Item) REFERENCES item(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (id_Registro),
+    FOREIGN KEY (id_Item) REFERENCES item(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE compra (
     id INT NOT NULL UNIQUE,
     RFC_proveedor INT NOT NULL UNIQUE,
     Marca VARCHAR(50) CHECK(Marca <> ''),
-    Tipo VARCHAR(50) CHECK(Tipo <> ''),
-    FechaCompra DATE NOT NULL CHECK(FechaCompra <= NOW()),
-    FechaCaducidad DATE NOT NULL CHECK(FechaCaducidad <= NOW()),
+    Tipo  VARCHAR(50) CHECK(Tipo <> ''),
+    FechaCompra     DATE NOT NULL CHECK(FechaCompra <= NOW()),
+    FechaCaducidad  DATE NOT NULL CHECK(FechaCaducidad <= NOW()),
     CantidadComprada INT NOT NULL CHECK(CantidadComprada >= 0),
     PrecioCompra DECIMAL NOT NULL CHECK(PrecioCompra >= 0),
 
-    PRIMARY KEY(id),
-    FOREIGN KEY(RFC_proveedor) REFERENCES proveedor(RFC) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (id),
+    FOREIGN KEY (RFC_proveedor) REFERENCES proveedor(RFC) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE proveedor (
-    RFC VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
+    RFC    VARCHAR(13) NOT NULL CHECK(RFC ~* '^[a-z0-9]+$') UNIQUE,
     Nombre VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre ~* '^[a-z]+$'),
-    Servicio TEXT NOT NULL,
+    Servicio TEXT      NOT NULL,
 
-    PRIMARY KEY(RFC)
+    PRIMARY KEY (RFC)
 );
 
 CREATE TABLE telefono_proveedor(
     RFC_proveedor VARCHAR(13) NOT NULL CHECK(RFC_proveedor ~* '^[a-z0-9]+$') UNIQUE,
-    Telefono VARCHAR(10)  NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,  
+    Telefono      VARCHAR(10) NOT NULL CHECK(CHAR_LENGTH(Telefono) = 10) UNIQUE,  
 
     PRIMARY KEY(RFC_proveedor, Telefono),
     FOREIGN KEY(RFC_proveedor) REFERENCES proveedor(RFC) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE email_proveedor(
-    RFC_proveedor VARCHAR(13) NOT NULL CHECK(RFC_proveedor ~* '^[a-z0-9]+$') UNIQUE,
-    Email VARCHAR(320) NOT NULL UNIQUE,
+    RFC_proveedor VARCHAR(13)  NOT NULL CHECK(RFC_proveedor ~* '^[a-z0-9]+$') UNIQUE,
+    Email         VARCHAR(320) NOT NULL UNIQUE,
     
-
-    PRIMARY KEY(RFC_proveedor, Email),
-    FOREIGN KEY(RFC_proveedor) REFERENCES proveedor(RFC) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (RFC_proveedor, Email),
+    FOREIGN KEY (RFC_proveedor) REFERENCES proveedor(RFC) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE abastecer (
     id_Taqueria INT NOT NULL UNIQUE,
-    id_Compra INT NOT NULL UNIQUE,
+    id_Compra   INT NOT NULL UNIQUE,
 
-    PRIMARY KEY(id_Taqueria, id_Compra),
-    FOREIGN KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY(id_Compra) REFERENCES compra(id) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (id_Taqueria, id_Compra),
+    FOREIGN KEY (id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (id_Compra) REFERENCES compra(id) ON UPDATE CASCADE ON DELETE CASCADE
 
 );
 
@@ -368,11 +371,11 @@ CREATE TABLE inventario (
     id_Taqueria INT NOT NULL UNIQUE,
 
     Tipo VARCHAR(50) CHECK(Tipo <> ''),
-    FechaCaducidad DATE NOT NULL,
+    FechaCaducidad  DATE NOT NULL,
     CantidadRestante INT NOT NULL CHECK(CantidadRestante >= 0),
 
-    PRIMARY KEY(id)
-    FOREIGN KEY(id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (id)
+    FOREIGN KEY (id_Taqueria) REFERENCES taqueria(id) ON UPDATE CASCADE ON DELETE CASCADE,
 );
 
 
