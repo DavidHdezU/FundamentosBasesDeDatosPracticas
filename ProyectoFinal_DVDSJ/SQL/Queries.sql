ac3057bb7e441146a81c1c90d2dbdc46fff388d8
DavidHdezU <Dhdezu@ciencias.unam.mx>
2022-06-14 20:09:21 -0500

    Queries v4

--- ProyectoFinal_DVDSJ/SQL/Queries.sql
+++ ProyectoFinal_DVDSJ/SQL/Queries.sql
@@ -42,15 +42,14 @@
 SELECT CURP, Nombre, A_Paterno, count(CURP) "Total de compras", SUM(Total) "Gastos totales"
 FROM cliente
 INNER JOIN ticket ON cliente.CURP=ticket.id_Cliente
-group by cliente.CURP
-order by 4 desc
+GROUP BY cliente.CURP
+ORDER BY 4 desc
 LIMIT 15;
 
--- INGRESOS TOTALES DE LAS CON INGRESOS MAYORES O IGUALES A 1000
-SELECT id_Taqueria, SUM(Total) "Ingresos" FROM ticket left join cliente ON ticket.id_Cliente = cliente.CURP
+-- INGRESOS TOTALES DE LAS 
+SELECT id_Taqueria, SUM(Total) "Ingresos" FROM ticket
 GROUP BY id_Taqueria
-HAVING SUM(Total) >= 1000
-ORDER BY 2 desc;
+ORDER BY 1;
 
 -- EMPLEADOS QUE CUMPLEN CON EL PROGRAMA DE ANTIGUEDAD
 SELECT CURP, id_Taqueria, Nombre, A_Paterno, Antiguedad FROM taquero
@@ -74,10 +73,24 @@ HAVING DATE_PART('year', AGE(NOW(), antiguedad)) >= 2;
 
 
 -- NUMERO DE CLIENTES ATENDIDOS POR SUCURSAL
-SELECT id_taqueria, COUNT(id_taqueria) "Clientes atendidos totales" FROM cliente
+SELECT id_taqueria, COUNT(id_taqueria) "Clientes atendidos totales" FROM ticket
 GROUP BY id_taqueria
-ORDER BY 2 desc;
+ORDER BY 2 DESC;
 
+-- 20 MESEROS CON MÁS CLIENTES ATENDIDOS
+SELECT RFC, Nombre, A_Paterno, A_Materno, Telefono, Email, COUNT(RFC_mesero) "Clientes atendidos" 
+FROM mesero INNER JOIN ticket ON mesero.RFC = ticket.RFC_mesero
+GROUP BY RFC, Nombre, A_Paterno, A_Materno, Telefono, Email
+ORDER BY 7 DESC
+LIMIT 20;
+
+-- LAS 10 TAQUERIAS QUE HAN TENIDO QUE ABASTECERSE MÁS EN EL ULTIMO MES (BETWEEN 2022-05-14 2022-06-14)
+
+SELECT id_taqueria, SUM(PrecioCompra) "Gastos totales en abastencimiento"
+FROM abastecer INNER JOIN compra ON compra.id = abastecer.id_Compra
+GROUP BY id_taqueria
+ORDER BY 2 DESC
+LIMIT 10;
 
 
 
