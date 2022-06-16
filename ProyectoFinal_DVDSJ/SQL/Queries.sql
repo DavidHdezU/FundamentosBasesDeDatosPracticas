5b2e42be5dd81c7824a50204c2cbb53dfd538024
DiXap <dpadlara@ciencias.unam.mx>
2022-06-15 23:20:16 -0500

    Borrando comentarios de Queries.sql

--- ProyectoFinal_DVDSJ/SQL/Queries.sql
+++ ProyectoFinal_DVDSJ/SQL/Queries.sql
@@ -1,43 +1,3 @@
-/*
- * Ideas
-
-    - Consultar que items son los que aparecen más 
-
-    - Ver que clientes son los más frecuentes mediante los tickets
-
-    - Ver cuantos empleados cumplen con el programa de pagos
-
-    - Ver cuantos empleados también son clientes
-
-    - Ver que que proveedores son lo más frecuentes
-
-    - Ver que ingredientes se usan en salsas con un nivel de picos 'n'
-
-    - Cumulativo de ingresos acumulados por taqueria
-
-    - Ver que meseros han sido los que han atendido más clientes
-
-    - Ver el acumulativo total de los pagos hecho en tarjeta
-
-    - Ver que item se vendió más en cierto día
-
-    - Ver que item se vendió más en una taqueria en un cierto periodo de tiempo
-
-    - Items con un avg(precio) > 'n' sobre el registro historico
-
-
-    - Ver cuantos tacos al pastor se venden y el ingreso generado los lunes en cada taqueria en un mes
-
-    - Ver que taco es el más vendido en las taquerias
-
-    - Ver que ingredientes se usan en la salsa con el maximo picor
-
-
- * 
- */
-
-
-
 -- OBTENER LOS 15 CLIENTES MÁS FRECUENTES
 SELECT CURP, Nombre, A_Paterno, count(CURP) "Total de compras", SUM(Total) "Gastos totales"
 FROM cliente
@@ -254,6 +214,7 @@ ON ticket.id = foo.id_ticket
 WHERE id_taqueria=1
 GROUP BY id_taqueria,nombre
 ORDER BY total DESC;
+
 
 -- TACOS MAS VENDIDOS POR SUCURSAL  
 SELECT 
